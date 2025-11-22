5fbd0a74890ad2a6daef3a4f5e8f5f6a9cf19f62
Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>
2025-11-22 13:34:53 +0100

    Update to Prisma 7 and add Prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
-}
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter });
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+// const globalForPrisma = globalThis as unknown as {
+//     prisma: PrismaClient | undefined
+// }
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
